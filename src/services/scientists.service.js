8b87450e1794564f5c65a44a7a7eefa1c9c1e9e7
Иван <ivanmatuyhin@gmail.com>
2024-05-16 14:10:31 +0300

    change routes at scientist API

--- src/services/scientists.service.js
+++ src/services/scientists.service.js
@@ -2,23 +2,23 @@ import { Api } from './api.service';
 
 export const ScientistsService = {
     getScientists: async () => {
-        const response = await Api.get('/api/scientists/all');
+        const response = await Api.get('/api/scientists');
         return response.data;
     },
     getScientistsNames: async () => {
-        const response = await Api.get('/api/scientists/all/names');
+        const response = await Api.get('/api/scientists/names');
         return response.data;
     },
     createScientist: async (data) => {
-        const response = await Api.post('/api/scientists/create', data);
+        const response = await Api.post('/api/scientists', data);
         return response.data;
     },
     updateScientist: async (id, data) => {
-        const response = await Api.put(`/api/scientists/update/${id}`, data);
+        const response = await Api.put(`/api/scientists/${id}`, data);
         return response.data;
     },
     deleteScientist: async (id) => {
-        const response = await Api.delete(`/api/scientists/delete/${id}`);
+        const response = await Api.delete(`/api/scientists/${id}`);
         return response.data;
     }
 };
